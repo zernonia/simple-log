83d0d481977a0e15d26e6f399b62a3bc3442d9aa
zernonia <zernonia@gmail.com>
2022-08-20 00:00:18 +0800

    add back fetch event to test the prompt showing on andriod or not

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,17 @@
-self.addEventListener("fetch", (e) => {})
+self.addEventListener("fetch", function (event) {
+  event.respondWith(
+    (async function () {
+      try {
+        var res = await fetch(event.request, { cache: "no-store" })
+        var cache = await caches.open("cache")
+        cache.put(event.request.url, res.clone())
+        return res
+      } catch (error) {
+        return caches.match(event.request)
+      }
+    })()
+  )
+})
 
 self.addEventListener("push", (event) => {
   let notification = event.data.json()
