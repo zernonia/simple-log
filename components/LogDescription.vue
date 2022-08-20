3cd24aac9dfee5bb6c9bb98ecc784ef7c0ff71aa
zernonia <zernonia@gmail.com>
2022-08-20 15:20:13 +0800

    fix json string too wide

--- components/LogDescription.vue
+++ components/LogDescription.vue
@@ -18,14 +18,14 @@ const isStringParsable = (text: string) => {
 <template>
   <div>
     <div v-if="isStringParsable(description)" class="p-4 rounded-xl bg-gray-50 overflow-y-auto">
-      {{ convertJSONToString(description) }}
-      <!-- <p v-for="(value, key) in JSON.parse(description)">
+      <!-- {{ convertJSONToString(description) }} -->
+      <p v-for="(value, key) in JSON.parse(description)">
         {{ key }}:  
         <p class="ml-4" v-if="typeof value === 'object'" v-for="(child_value, child_key) in value">
           {{ child_key }}: {{ child_value }}
         </p>
         <span v-else>{{ value }}</span>
-      </p> -->
+      </p>
     </div>
     <div v-else class="whitespace-pre-wrap">{{ description }}</div>
   </div>
