84a69c67ec234a4550c34bac879b86340dcfc19a
zernonia <zernonia@gmail.com>
2022-08-24 09:19:09 +0800

    add loading to button when finish redirect

--- pages/login.vue
+++ pages/login.vue
@@ -15,11 +15,24 @@ watch(
   },
   { immediate: true }
 )
+
+const { hash } = toRefs(useRoute())
+watch(
+  hash,
+  (n) => {
+    if (n)
+      nextTick(() => {
+        isLoading.value = true
+      })
+  },
+  { immediate: true }
+)
 </script>
 
 <template>
   <div class="flex flex-col items-center mt-20">
     <h2 class="text-5xl font-extrabold">Login</h2>
+    {{ isLoading }}
 
     <ButtonLoader :loading="isLoading" class="btn-primary mt-8" @click="signIn">
       <div class="i-uil-github mr-2 text-xl"></div>
